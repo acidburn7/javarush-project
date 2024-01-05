0806136bcd077b866997e73824fe5a2b99def77a
acidburn7 <varaksinaalla@mail.ru>
2024-01-05 23:59:16 +0300

    solved problem

---
+++ src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import javax.security.sasl.SaslClient;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        String one = new String("one");
+        String two = new String("two");
+        String three = new String("three");
+        String four = new String("four");
+        String five = new String("five");
+        String six = new String("six");
+        String seven = new String("seven");
+        String eight = new String("eight");
+        String nine = new String("nine");
+        String ten = new String("ten");
+
+        int[] intone = new int[10];
+        int[] dd = new int[10];
+        int[] ww = new int[10];
+        int[] rr = new int[10];
+        int[] qwq = new int[10];
+
+        Scanner sc1 = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+    }
+}
